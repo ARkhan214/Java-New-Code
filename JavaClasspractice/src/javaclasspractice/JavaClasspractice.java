ab69a6dde8ab96b11af5ad8d3b2536f9eb1501b8
ARkhan214 <abdurrahimkhan214dc@gmail.com>
2025-04-22 22:14:49 +0600

    add

---
+++ JavaClasspractice/src/javaclasspractice/JavaClasspractice.java
@@ -0,0 +1,23 @@
+
+package javaclasspractice;
+
+import javaclasspractice.student.NewClassStudent;
+
+
+public class JavaClasspractice {
+
+   
+    public static void main(String[] args) {
+        NewClassStudent input=new NewClassStudent();
+        
+        input.setName("Babor");
+        input.setStudentID("127890");
+        input.setDepartment("Political Science");
+        
+        
+        System.out.println(input);
+        
+        
+    }
+    
+}
---
+++ JavaClasspractice/src/javaclasspractice/student/NewClassStudent.java
@@ -0,0 +1,54 @@
+
+package javaclasspractice.student;
+
+
+public class NewClassStudent {
+    private String name;
+    private String studentID;
+    private String department;
+
+    public NewClassStudent() {
+    }
+
+    public NewClassStudent(String name, String studentID, String department) {
+        this.name = name;
+        this.studentID = studentID;
+        this.department = department;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStudentID() {
+        return studentID;
+    }
+
+    public void setStudentID(String studentID) {
+        this.studentID = studentID;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    @Override
+    public String toString() {
+        return "NewClassStudent{" + "name=" + name + ", "
+                + "studentID=" + studentID + ", "
+                + "department=" + department + '}';
+    }
+    
+    
+    
+    
+    
+}
