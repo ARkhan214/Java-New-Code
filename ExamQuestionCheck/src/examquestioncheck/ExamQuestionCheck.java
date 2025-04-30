013473435f2192fa0eddda6d8c7cd2278ce294e2
ARkhan214 <abdurrahimkhan214dc@gmail.com>
2025-04-30 14:21:00 +0600

    add

---
+++ ExamQuestionCheck/src/examquestioncheck/ExamQuestionCheck.java
@@ -0,0 +1,59 @@
+package examquestioncheck;
+
+import java.util.Arrays;
+
+public class ExamQuestionCheck {
+
+    public static void main(String[] args) {
+        
+        
+
+
+        
+    
+
+    //        int i = 1;
+    //        do {
+    //            i--;
+    //        } while (i > 2);
+    //
+    //        System.out.println(i);
+    //	int [ ] a [ ] = new int [4] [4];  
+    //                System.out.println(a);
+    //
+    //	int b[ ][ ] = new int [4] [4];  
+    //                System.out.println(b);
+    //
+    ////	int c[ ] [ ] = new int [ ] [4]; 
+    ////        System.out.println(c);
+    //        int [ ] d [ ] = new int [4] [ ];  
+    //
+    //        System.out.println(Arrays.toString(d));
+//        int i = 0;
+//        while (true) {
+//            if (i == 4) {
+////                System.out.println(i);
+//                break;
+//            }
+//            ++i;
+//        }
+//        System.out.println(" i=" + i);
+//Question ----41----
+//        int i = 0;
+//        for (; i < 4; i += 2) {
+//            System.out.print(i
+//                    + "");
+//        }
+//        System.out.println(i);
+//        int i = 2, j = 11;
+//        do {
+//            if (i > j) {
+//                break;
+//            }
+//            j--;
+//        } 
+//        while (++i < 6);
+//        System.out.println("i = " + i + " and j = " + j);
+}
+
+}
