279384527916ee558870bd309f6301e6011db5ed
ARkhan214 <abdurrahimkhan214dc@gmail.com>
2025-05-03 15:16:01 +0600

    add

--- GitCheck/src/gitcheck/GitCheck.java
+++ GitCheck/src/gitcheck/GitCheck.java
@@ -11,11 +11,13 @@ public class GitCheck {
       Scanner input=new Scanner(System.in);
         System.out.println("Enter Number");
         float number=input.nextFloat();
-        if (number%2==0 && number>=0) {
+        if (number>0) {
+            if (number%2==0 && number>=0) {
             System.out.println(number+" is Even Number");
         }
         else{System.out.println(number+" is Odd Number.");}
-        
+        }
+        else{System.out.println(number+" is negative number");}
         
         
         
