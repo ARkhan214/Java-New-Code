8b4ca88a81a9d5dcece68c90d04da8dfbf7be273
MD RAHIM KHAN <abdurrahimkhan214dc@gmail.com>
2025-05-17 19:03:54 +0600

    add

---
+++ POSproject/src/posproject/POSproject.java
@@ -0,0 +1,18 @@
+
+package posproject;
+
+import posproject.util.POSAddView;
+
+
+public class POSproject {
+
+   
+    public static void main(String[] args) {
+       
+        POSAddView pav = new POSAddView();
+        pav.setVisible(true);
+        
+        
+    }
+    
+}
