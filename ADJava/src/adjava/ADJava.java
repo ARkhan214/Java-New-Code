0bdafaadb0f2daf39d67b44d82154da5f9c23963
MD RAHIM KHAN <abdurrahimkhan214dc@gmail.com>
2025-05-29 18:53:53 +0600

    add

---
+++ ADJava/src/adjava/ADJava.java
@@ -0,0 +1,14 @@
+
+package adjava;
+
+
+public class ADJava {
+
+    
+    public static void main(String[] args) {
+       
+        
+        
+    }
+    
+}
---
+++ ADJava/src/dao/PurchaseDao.java
@@ -0,0 +1,19 @@
+
+package dao;
+
+import java.util.Date;
+
+
+public class PurchaseDao {
+    
+    private int id;
+    private String name;
+    private float unitPrice;
+    private float quantity;
+    private float totalPrice;
+    private Date date;
+    private String category;
+    private String supplier;
+    
+    
+}
